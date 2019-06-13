6e27a3e6ff05acbccd591dbe61310a58da274306
Pedro Freire <pedro-of@live.com>
2019-06-12 23:46:52 -0300

    react + routes e configurações

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+
+import Routes from './routes';
+
+function App() {
+  return (
+    <div className="App">
+        <BrowserRouter>
+            <Routes />
+        </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
